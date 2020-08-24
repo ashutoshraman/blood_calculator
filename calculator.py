d318f0d700db69a2c0f5b703af8264c2c3bfff19
Ashutosh Raman <ashutosh.raman@duke.edu>
2020-08-24 12:06:13 -0500

    User enters HDL test in console

--- calculator.py
+++ calculator.py
@@ -2,10 +2,20 @@ def interface():
     print("My Program")
     while True:  #evaluate while condition is true, only leaves when return command is found
         print("Options")
+        print("1-HDL")
         print("9-Quit")
         choice = input("Enter your choice:")
         if choice == '9':
             return
         elif choice == '1':
-              HDL_driver()
+            HDL_driver()
+
+def HDL_driver():
+    #get input
+    HDL_result= get_HDL_result()
+    #check if hdl is normal
+    #output
+def get_HDL_input():
+    HDL_input= input("enter the HDL test result:")
+    return int(HDL_input)
 interface()
