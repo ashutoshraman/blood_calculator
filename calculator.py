80d380ddb2ac723cae1a0890c30534e389265567
Ashutosh Raman <ashutosh.raman@duke.edu>
2020-08-24 22:12:40 -0500

    Added option for interpreting LDL value

--- calculator.py
+++ calculator.py
@@ -3,12 +3,15 @@ def interface():
     while True:  #evaluate while condition is true, only leaves when return command is found
         print("Options")
         print("1-HDL")
+        print("2-LDL")
         print("9-Quit")
         choice = input("Enter your choice:")
         if choice == '9':
             return
         elif choice == '1':
             HDL_driver()
+        elif choice == '2':
+            LDL_driver()
 
 def HDL_driver():
     #get input
@@ -21,8 +24,8 @@ def get_HDL_input():
     HDL_input= input("enter the HDL test result:")
     return int(HDL_input)
 def output_HDL(test_result, analysis):
-    print ("HDL result is {}".format(test_result))
-    print("HDL result is {}".format(analysis))
+    print ("result is {}".format(test_result))
+    print("result is {}".format(analysis))
 def check_HDL(testvalue):
     if testvalue >=60:
         return "Normal"
@@ -30,4 +33,20 @@ def check_HDL(testvalue):
         return "borderline low"
     else:
         return "low"
+def LDL_driver():
+    LDL_input= get_LDL_input()
+    LDL_interp= check_LDL(LDL_input)
+    output_HDL(LDL_input, LDL_interp)
+def get_LDL_input():
+    inputLDL= input("Enter LDL result:")
+    return int(inputLDL)
+def check_LDL(testvalue):
+    if testvalue <=130:
+        return "Normal"
+    elif 130<= testvalue < 159:
+        return "borderline high"
+    elif 160 <= testvalue <189:
+        return "high"
+    else:
+        return "too damn high"
 interface()
